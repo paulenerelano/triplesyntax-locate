0e314bf01b748e048ba36f4dce47db6b04d69fa3
paulenerelano <paulene.relano@gmail.com>
2018-10-18 07:16:01 +0800

    aaa

--- testmap/views/dashboard.py
+++ testmap/views/dashboard.py
@@ -6,9 +6,11 @@ from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 import json
 
-
+from testmap.models.userlocation import UserLocation
 
 class DashboardView(View):
+
+    dashboard_template = 'dashboard/index.html'
 
     def get(self, request, *args, **kwargs):
         return render(request, self.dashboard_template)
@@ -25,6 +27,16 @@ class DashboardView(View):
             print(err.args)
             return HttpResponse(status=500)
 
-        print(body_json)
-
+        # long = float(body_json["long"])
+        # lat = float(body_json["lat"])
+        # user_location = UserLocation(id=1, name='triplesyntax', longitude=long, latitude=lat)
+        #
+        # user_location.save()
         return HttpResponse(status=200)
+
+    # @classmethod
+    # def get_coords(cls, request):
+    #     cur_loc = UserLocation.objects.get(id=1)
+    #     rsp = [{"long":cur_loc.longitude, "latitude":cur_loc.latitude}]
+    #
+    #     return HttpResponse(status=200, content_type="application/json", content=json.dumps(rsp))
